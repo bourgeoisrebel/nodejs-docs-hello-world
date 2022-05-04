35c3a30798e0c4b86b422cc6182648b68a383ff1
bourgeoisrebel <bourgeoisrebel@users.noreply.github.com>
2022-05-04 18:24:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello UK South!");
+    response.end("Hello UK West!");
 });
 
 const port = process.env.PORT || 1337;
